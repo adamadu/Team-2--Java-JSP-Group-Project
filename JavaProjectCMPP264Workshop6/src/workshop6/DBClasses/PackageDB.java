690f793a9ab1c26079d34edee2409d00534ff57c
Geetham15 <Geetham15@gmail.com>
2015-09-03 14:36:50 -0600

    Package db methods

--- JavaProjectCMPP264Workshop6/src/workshop6/DBClasses/PackageDB.java
+++ JavaProjectCMPP264Workshop6/src/workshop6/DBClasses/PackageDB.java
@@ -2,11 +2,11 @@ package workshop6.DBClasses;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.List;
 import java.util.Vector;
 import workshop6.Entity.Package;
-import workshop6.Entity.ProductSupplier;
+
 
 import workshop6.DBClasses.DatabaseConnection;
 
@@ -16,65 +16,132 @@ public class PackageDB {
 	private static Statement stmt;
 	private static Connection conn;
 	private static Package pkg;
-	private static List<ProductSupplier> productSupplier;
+	
 	
 	//Method to get all Package ID
-		public static Vector<String> getPackageIDs()
-		{
-			Vector<String> pkgid = new Vector<String>();
-			try
-			{
-				conn = DatabaseConnection.getConnection();
-				stmt =conn.createStatement();
-				rs = stmt.executeQuery("select packageId from Packages");
-				while(rs.next())
-				{
-					pkgid.add(rs.getString("packageId"));
-				}
-				conn.close();
-			}
-			catch(Exception ex)
-			{
-				ex.printStackTrace();
-			}
-			return pkgid;
-		}
+        public static Vector<String> getPackageIDs()
+        {
+                Vector<String> pkgid = new Vector<String>();
+                try
+                {
+                        conn = DatabaseConnection.getConnection();
+                        stmt =conn.createStatement();
+                        rs = stmt.executeQuery("select packageId from Packages");
+                        while(rs.next())
+                        {
+                                pkgid.add(rs.getString("packageId"));
+                        }
+                        conn.close();
+                }
+                catch(SQLException ex)
+                {
+                System.out.println("Error occured while retrieving packageids: " + ex.getMessage());
+                }
+                catch(Exception ex)
+                {
+                        ex.printStackTrace();
+                }
+                return pkgid;
+        }
 		
-		//Method to get the package details for the particular package
-		public static Package getPackage(String packageId)
-		{
-			try{
-				conn = DatabaseConnection.getConnection();
-				stmt =conn.createStatement();
-				rs = stmt.executeQuery("select * from Packages where packageId = " + packageId);
-				if(rs.next())
-				{
-					//Agent agent = new Agent();
-					pkg = new Package();
-					pkg.setPackageId(rs.getInt(1));
-					pkg.setPkgName(rs.getString(2));
-					pkg.setPkgStartDate(rs.getDate(3));
-					pkg.setPkgEndDate(rs.getDate(4));
-					pkg.setPkgDesc(rs.getString(5));
-					pkg.setPkgBasePrice(rs.getDouble(6));
-					pkg.setPkgAgencyCommission(rs.getDouble(7));
-				}
-				conn.close();
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-			return pkg;
-		}
+        //Method to get the package details for the particular package
+        public static Package getPackage(String packageId)
+        {
+                try{
+                        conn = DatabaseConnection.getConnection();
+                        stmt =conn.createStatement();
+                        rs = stmt.executeQuery("select * from Packages where packageId = " + packageId);
+                        if(rs.next())
+                        {
+                                //Agent agent = new Agent();
+                                pkg = new Package();
+                                pkg.setPackageId(rs.getInt(1));
+                                pkg.setPkgName(rs.getString(2));
+                                pkg.setPkgStartDate(rs.getDate(3));
+                                pkg.setPkgEndDate(rs.getDate(4));
+                                pkg.setPkgDesc(rs.getString(5));
+                                pkg.setPkgBasePrice(rs.getDouble(6));
+                                pkg.setPkgAgencyCommission(rs.getDouble(7));
+                        }
+                        conn.close();
+                }
+                catch(SQLException ex)
+                {
+                System.out.println("Error occured while retrieving package details: " + ex.getMessage());
+                }
+                catch(Exception e){
+                        e.printStackTrace();
+                }
+                return pkg;
+        }
 
-		/*public static List<ProductSupplier> getProductSupplier(String packageId)
-		{
-			List<ProductSupplier> productSupplier = new List<ProductSupplier>();
-			
-			String qrySelect = "SELECT ps.ProductSupplierId, ps.ProductId, p.ProdName, ps.SupplierId, s.SupName " +
-	                "FROM Packages_Products_Suppliers pps, Products_Suppliers ps, Products p, Suppliers s " +
-	                "WHERE pps.ProductSupplierId = ps.ProductSupplierId AND ps.ProductId = p.ProductId " + 
-	                "AND ps.SupplierId = s.SupplierId AND pps.PackageId = " + packageId;
-			
-			return productSupplier;
-		}*/
+        //Method to add package details
+        public static boolean addPackage(Package pkg)
+        {
+                try
+                {
+                        conn = DatabaseConnection.getConnection();
+                        stmt =conn.createStatement();
+                        System.out.println("PackageId : " + pkg.getPackageId());
+                        String addPackage = "Insert Into Package (PkgName, PkgStartDate, PkgEndDate, PkgDesc, PkgBasePrice, PkgAgencyCommission) Values "
+                                        + "(pkg.getPkgName(), pkg.getPkgStartDate(),pkg.getPkgEndDate(),pkg.getPkgDesc(),pkg.getPkgBasePrice(),pkg.getPkgAgencyCommission())";		
+                        int numRows = stmt.executeUpdate(addPackage);
+                        if (numRows == 0)
+                        {
+                                System.out.println("Package not inserted");
+                                return false;
+                        }
+                        else
+                            return true;
+                        //conn.close();
+                }
+                catch(SQLException e)
+                {
+                        e.printStackTrace();
+                        return false;
+                }
+        }
+                
+        //Method to update Package
+        public static boolean updatePackage(Package pkg)
+        {
+            try
+            {
+                conn = DatabaseConnection.getConnection();
+                stmt =conn.createStatement();
+                String updatePackage = "Update Package set " +
+                                        "PkgName='" + pkg.getPkgName() +
+                                        "',PkgStartDate='" + pkg.getPkgStartDate() +
+                                        "',PkgEndDate='" + pkg.getPkgEndDate() +
+                                        "',PkgDesc='" + pkg.getPkgDesc() +
+                                        "',PkgBasePrice='" + pkg.getPkgBasePrice() +
+                                        "',PkgAgencyCommission='" + pkg.getPkgAgencyCommission() +
+                                        "' where packageId='" + pkg.getPackageId()+"'";                
+
+                int numRows = stmt.executeUpdate(updatePackage);
+                conn.close();
+                if (numRows == 0)
+                {
+                        System.out.println("Update package failed");
+                        return false;
+                }
+                else
+                {
+                        System.out.println("updated " + numRows + " row(s) in Package");
+                        return true;
+                }
+                
+            }
+            catch(SQLException ex)
+            {
+                System.out.println("Error occured while updating package: " + ex.getMessage());
+                return false;
+            }
+            catch(Exception ex)
+            {
+                ex.printStackTrace();
+                return false;
+            }
+        }
+                
 }
